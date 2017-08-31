13c7fc023ed26c299d8850a9f0c204387faa4c93
Antony Budianto <antonybudianto@gmail.com>
2017-08-31 12:30:26 +0700

    add jest and unit test, with travis ci

---
+++ src/universal.spec.js
@@ -0,0 +1,81 @@
+process.env.NODE_ENV = 'production';
+
+const universalMiddleware = require('./universal');
+jest.mock('fs');
+const fs = require('fs');
+
+test('have correct env', () => {
+  expect(process.env.NODE_ENV).toBe('production');
+});
+
+test('created correctly', () => {
+  const config = {
+    clientBuildPath: 'test',
+    universalRender: () => {}
+  };
+  const middleware = universalMiddleware(config);
+  expect(middleware).toBeDefined();
+});
+
+test('read index.html file correctly', () => {
+  const config = {
+    clientBuildPath: 'test',
+    universalRender: () => {}
+  };
+  const middleware = universalMiddleware(config);
+  middleware();
+  expect(fs.readFile).toHaveBeenCalledTimes(1);
+});
+
+test('handle read error correctly', () => {
+  const config = {
+    clientBuildPath: 'test',
+    universalRender: () => {}
+  };
+  const middleware = universalMiddleware(config);
+  jest.spyOn(fs, 'readFile').mockImplementation((filepath, enc, callback) => {
+    const err = { message: 'mock readFile error' };
+    const htmlData = null;
+    callback(err, htmlData);
+  });
+  const spy = jest.spyOn(console, 'error');
+  const mockStatus = {
+    end: jest.fn()
+  };
+  const mockResponse = {
+    status: jest.fn(() => mockStatus)
+  };
+  middleware({}, mockResponse);
+  expect(mockResponse.status).toHaveBeenCalledWith(404);
+  expect(console.error).toHaveBeenCalledTimes(1);
+
+  spy.mockReset();
+  spy.mockRestore();
+});
+
+test('send response successfully', () => {
+  const config = {
+    clientBuildPath: 'test',
+    universalRender: () => '<div>test</div>'
+  };
+  const middleware = universalMiddleware(config);
+  jest.spyOn(fs, 'readFile').mockImplementation((filepath, enc, callback) => {
+    const htmlData = '<html><div id="root"></div></html>';
+    callback(null, htmlData);
+  });
+  const spy = jest.spyOn(console, 'error');
+  const mockStatus = {
+    end: jest.fn()
+  };
+  const mockResponse = {
+    send: jest.fn()
+  };
+  middleware({}, mockResponse);
+  expect(mockResponse.send).toHaveBeenCalledWith(
+    `<html><div id="root"><div>test</div></div></html>`
+  );
+  expect(console.error).toHaveBeenCalledTimes(0);
+
+  spy.mockReset();
+  spy.mockRestore();
+});
